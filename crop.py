b8d6391767ba3a0888f260c31a1c82eb55eee8da
wang-ting000 <2777103910@qq.com>
2021-02-27 07:52:30 +0800

    some fixes

--- crop.py
+++ crop.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+import pandas as pd
 
 im = cv2.imread('chess.png')
 
@@ -18,25 +19,43 @@ def board_position(im,row,col):
     print(row_st,row_end,col_st,col_end)
 
     im_crop=im[row_st:row_end,col_st:col_end]
+
+
+
     cv2.imshow('cropped',im_crop)
 
     im_gray =cv2.cvtColor(im_crop,cv2.COLOR_BGR2GRAY)
 
-    n = np.size(im_gray)-np.count_nonzero(im_gray)
+    n = np.count_nonzero(im_gray)
     m = np.size(im_gray)
-    im_gray = [i-im_gray for i in im_gray ]
+    s = im_gray[0]
+    im_gray = [i-im_gray[0] for i in im_gray ]
     if np.count_nonzero(im_gray) == np.size(im_gray):
         #cv2.addText(im_crop,'empty',(300,300),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
         print('empty')
     else:
         white_pixel = int(n/m*100)
+        print(white_pixel)
         if white_pixel>80:
             #cv2.addText(im_crop,'white',(300,300),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
             print('white')
         else:
             #cv2.addText(im_crop,'black',(300,300),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
             print('black')
+    im_gray = [i + s for i in im_gray]
 
     cv2.waitKey(0)
+    print(np.shape(im_gray))
+    return im_gray
 
-board_position(im,7,1)
+
+'''a = board_position(im,7,1)
+df = pd.DataFrame(a)
+writer = pd.ExcelWriter('hahaha.xlsx')
+df.to_excel(writer,'page_1')
+writer.save()'''
+a = board_position(im, 7, 1)
+df = pd.DataFrame(a)
+writer = pd.ExcelWriter('hahaha.xlsx')
+df.to_excel(writer, 'page_1')
+writer.save()
